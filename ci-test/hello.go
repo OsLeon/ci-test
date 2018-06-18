708d6c63e7be12569704a95ddba4cb4b1f550efd
leon shi <cloudleon0128@gmail.com>
2018-06-18 21:15:07 +0800

    Update hello.go

--- ci-test/hello.go
+++ ci-test/hello.go
@@ -3,5 +3,5 @@ import "fmt"
 func main()
 {
  fmt.Println("hello,cloud!")
- fmt.Println("sdfdsfdsf你好，AWS90sdfdsfsdfdsfdsfdsfdsfdssdf000095325235235999")
+ fmt.Println("sdfdsfdsf你好，AWS90sdfdsfssdfdsfddfdsfdsfdsfdsfdssdf000095325235235999")
 }
