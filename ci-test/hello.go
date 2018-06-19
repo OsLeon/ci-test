602ef6f4db98ea61a7481ee5e98bc05fe8de7953
leon shi <cloudleon0128@gmail.com>
2018-06-19 16:19:24 +0800

    Delete hello.go

--- ci-test/hello.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-func main()
-{
- fmt.Println("hello,cloud!")
- fmt.Println("sdfdsfdsf你好，AsdfsdfWS90sdsdsdsdsdfdsfssdfdsfddfdsfdssdfsdfdsfdsffdssssssfdsfdssdf000095325235235999")
-}
